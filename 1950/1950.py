3a476a3d3c7f0e10821f2c8f0feac89a6a6c8645
Kirill <73121729+Kirusland@users.noreply.github.com>
2024-03-30 14:50:50 +0300

    Create 1950.py

---
+++ 1950/1950.py
@@ -0,0 +1,11 @@
+counter = int(input())
+
+while (counter != 0):
+    a,b,c = map(int, input().split(' '))
+    if (a<b<c):
+        print("STAIR")
+    elif (a<b>c):
+        print("PEAK")
+    else:
+        print("NONE")
+    counter-=1
